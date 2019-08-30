402e38a16bf7fc9ea8ab00b68e3d78ee115c6bf9
David Buser <busdav@outlook.com>
2019-08-30 10:20:40 +0100

    Set up react app for production - one route at /app

---
+++ public/precache-manifest.2fba0a9b6024beec16b244813b1f17de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ac939a16434d02c32d5c3d08f0ada8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c001cd7a92b616d1ce44",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5d892d3bb2e1c34e04b5",
+    "url": "/static/js/2.46f455c8.chunk.js"
+  },
+  {
+    "revision": "c001cd7a92b616d1ce44",
+    "url": "/static/js/main.93c8dd1f.chunk.js"
+  },
+  {
+    "revision": "ad537bb9653274438d46",
+    "url": "/static/js/runtime~main.1cecb24d.js"
+  }
+]);
